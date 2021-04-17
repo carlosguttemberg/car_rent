2e5821ec601767493b241275cceabe71a5eaa034
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-04-17 14:32:01 -0300

    Criado as tipagens de categorias e metodos

--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -1,22 +1,28 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    name,
-    description,
-    id: uuidV4(),
-  };
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists" });
+  }
+
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
