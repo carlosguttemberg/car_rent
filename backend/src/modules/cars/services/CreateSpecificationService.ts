a01652baffd7af614266865404a71c55d284ff1a
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-04-25 16:24:52 -0300

    Criando o repositorio de especificacao

--- backend/src/modules/cars/services/CreateSpecificationService.ts
+++ backend/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("teste");
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificationsRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
