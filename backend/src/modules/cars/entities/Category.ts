e55762c2306c93556b2f7b32454bbe3c532392cc
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-06-19 14:35:27 -0300

    Criando as migrations, refatorando o modelo de categoria

--- backend/src/modules/cars/entities/Category.ts
+++ backend/src/modules/cars/entities/Category.ts
@@ -1,9 +1,18 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("categories")
 class Category {
+  @PrimaryColumn()
   id?: string;
+
+  @Column("name")
   name: string;
+
+  @Column("description")
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
