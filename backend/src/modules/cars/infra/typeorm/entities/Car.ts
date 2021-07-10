54945237a10cb787d7a6787ae66f41ecc31f9cde
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-07-10 16:50:22 -0300

    Continuando o caso de uso de carros

--- backend/src/modules/cars/infra/typeorm/entities/Car.ts
+++ backend/src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidV4 } from "uuid";
+
 class Car {
   id: string;
 
@@ -18,6 +20,14 @@ class Car {
   available: boolean;
 
   created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.available = true;
+      this.created_at = new Date();
+    }
+  }
 }
 
 export { Car };
