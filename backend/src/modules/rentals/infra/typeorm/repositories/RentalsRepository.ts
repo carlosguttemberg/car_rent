9ad134731d09358b47c1e9ea6e705aa10b14f4c4
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-08-29 15:35:33 -0300

    Criando a listagem de alugueis por usuario

--- backend/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ backend/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -54,6 +54,14 @@ class RentalsRepository implements IRentalsRepository {
     const rental = await this.repository.findOne(id);
     return rental;
   }
+
+  async findByUser(user_id: string): Promise<Rental[]> {
+    const rental = await this.repository.find({
+      user_id,
+    });
+
+    return rental;
+  }
 }
 
 export { RentalsRepository };
