157ca87be567321131b097633bd7e06e983aa886
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-09-11 14:22:05 -0300

    Refatorando a autenticacao de usuario

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "5476c61add7b12847f6f85f1cde59101",
+  expires_in_token: "15m",
+  secret_refresh_token: "26583336803dd9cee5f7fed8d1b13df5",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
