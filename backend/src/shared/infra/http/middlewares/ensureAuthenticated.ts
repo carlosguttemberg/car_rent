ed42d6d841720ff3b6248e4f2877345be9236ca6
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-09-16 12:31:17 -0300

    Corrigindo o refresh token

--- backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -17,7 +17,6 @@ export async function ensureAuthenticated(
   next: NextFunction
 ): Promise<void> {
   const authHeader = request.headers.authorization;
-  const userTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -26,19 +25,7 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      auth.secret_refresh_token
-    ) as IPayload;
-
-    const user = userTokensRepository.findByUserIdAndRefreshToken(
-      user_id,
-      token
-    );
-
-    if (!user) {
-      throw new AppError("User does not exists", 401);
-    }
+    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
     request.user = {
       id: user_id,
