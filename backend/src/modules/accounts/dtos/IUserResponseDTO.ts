92070d95c742f04fd567f9003ed53de080259163
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-09-28 20:38:59 -0300

    Criando a url de acesso do avatar

---
+++ backend/src/modules/accounts/dtos/IUserResponseDTO.ts
@@ -0,0 +1,10 @@
+interface IUserResponseDTO {
+  email: string;
+  name: string;
+  avatar: string;
+  id: string;
+  driver_license: string;
+  avatar_url(): string;
+}
+
+export { IUserResponseDTO };
