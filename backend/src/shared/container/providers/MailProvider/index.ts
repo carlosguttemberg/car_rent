e191601c54a0e553a25aa8e470b5bda241709d64
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-10-10 14:24:37 -0300

    Criando o provider do SESMail

---
+++ backend/src/shared/container/providers/MailProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from "tsyringe";
+
+import { IMailProvider } from "./IMailProvider";
+import { EtherealMailProvider } from "./implementations/EtherealMailProvider";
+import { SESMailProvider } from "./implementations/SESMailProvider";
+
+const mailStorage = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+container.registerInstance<IMailProvider>(
+  "MailProvider",
+  mailStorage[process.env.MAIL_PROVIDER]
+);
